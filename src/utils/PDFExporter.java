462d8bb5b119c9ca2cfd8bda7b0b67203a12851f
Vikas-Krishna1 <150981669+Vikas-Krishna1@users.noreply.github.com>
2026-06-10 20:40:25 -0500

    Fixed Exprt to Look better

--- src/utils/PDFExporter.java
+++ src/utils/PDFExporter.java
@@ -1,150 +1,336 @@
 package utils;
 
-import com.lowagie.text.*;
-import com.lowagie.text.pdf.PdfWriter;
-
 import models.LoanApplication;
 
 import java.io.FileOutputStream;
 
+import com.lowagie.text.Document;
+import com.lowagie.text.Element;
+import com.lowagie.text.Font;
+import com.lowagie.text.Paragraph;
+import com.lowagie.text.Phrase;
+
+import com.lowagie.text.pdf.PdfPCell;
+import com.lowagie.text.pdf.PdfPTable;
+import com.lowagie.text.pdf.PdfWriter;
+
 public class PDFExporter
 {
-    public static void exportApplication(
-            LoanApplication application,
-            String fileName)
-    {
-        try
-        {
-            Document document =
-                    new Document();
+public static void exportApplication(
+LoanApplication application,
+String fileName)
+{
+try
+{
+Document document =
+new Document();
+        PdfWriter.getInstance(
+                document,
+                new FileOutputStream(fileName));
 
-            PdfWriter.getInstance(
-                    document,
-                    new FileOutputStream(fileName));
+        document.open();
 
-            document.open();
+        Font titleFont =
+                new Font(
+                        Font.HELVETICA,
+                        18,
+                        Font.BOLD);
 
-            document.add(
-                    new Paragraph(
-                            "AUTO LOAN APPLICATION REPORT"));
+        Font sectionFont =
+                new Font(
+                        Font.HELVETICA,
+                        14,
+                        Font.BOLD);
 
-            document.add(
-                    new Paragraph(" "));
+        Paragraph title =
+                new Paragraph(
+                        "AUTO LOAN APPLICATION REPORT",
+                        titleFont);
 
-            document.add(
-                    new Paragraph(
-                            "Application ID: "
-                                    + application.getApplicationId()));
+        title.setAlignment(
+                Element.ALIGN_CENTER);
 
-            document.add(
-                    new Paragraph(
-                            "Status: "
-                                    + application.getStatus()));
+        document.add(title);
 
-            document.add(
-                    new Paragraph(" "));
+        document.add(new Paragraph(" "));
 
-            document.add(
-                    new Paragraph(
-                            "Applicant Information"));
+        // =====================================
+        // SUMMARY TABLE
+        // =====================================
 
-            document.add(
-                    new Paragraph(
-                            "Name: "
-                                    + application
-                                    .getApplicant()
-                                    .getFullName()));
+        PdfPTable summaryTable =
+                new PdfPTable(2);
 
-            document.add(
-                    new Paragraph(
-                            "Email: "
-                                    + application
-                                    .getApplicant()
-                                    .getEmail()));
+        summaryTable.setWidthPercentage(100);
 
-            document.add(
-                    new Paragraph(
-                            "Phone: "
-                                    + application
-                                    .getApplicant()
-                                    .getPhone()));
+        summaryTable.addCell(
+                headerCell("Application ID"));
 
-            document.add(
-                    new Paragraph(" "));
+        summaryTable.addCell(
+                valueCell(
+                        String.valueOf(
+                                application.getApplicationId())));
 
-            document.add(
-                    new Paragraph(
-                            "Vehicle Information"));
+        summaryTable.addCell(
+                headerCell("Status"));
 
-            document.add(
-                    new Paragraph(
-                            application
-                                    .getVehicle()
-                                    .getYear()
-                                    + " "
-                                    + application
-                                    .getVehicle()
-                                    .getMake()
-                                    + " "
-                                    + application
-                                    .getVehicle()
-                                    .getModel()));
+        summaryTable.addCell(
+                valueCell(
+                        application.getStatus()));
 
-            document.add(
-                    new Paragraph(" "));
+        document.add(summaryTable);
 
-            document.add(
-                    new Paragraph(
-                            "Loan Information"));
+        document.add(new Paragraph(" "));
 
-            document.add(
-                    new Paragraph(
-                            "Auto Price: $"
-                                    + application
-                                    .getLoan()
-                                    .getAutoPrice()));
+        // =====================================
+        // APPLICANT INFORMATION
+        // =====================================
 
-            document.add(
-                    new Paragraph(
-                            "Down Payment: $"
-                                    + application
-                                    .getLoan()
-                                    .getDownPayment()));
+        document.add(
+                new Paragraph(
+                        "Applicant Information",
+                        sectionFont));
 
-            document.add(
-                    new Paragraph(
-                            "Interest Rate: "
-                                    + application
-                                    .getLoan()
-                                    .getInterestRate()
-                                    + "%"));
+        PdfPTable applicantTable =
+                new PdfPTable(2);
 
-            document.add(
-                    new Paragraph(
-                            "Loan Term: "
-                                    + application
-                                    .getLoan()
-                                    .getLoanTerm()
-                                    + " months"));
+        applicantTable.setWidthPercentage(100);
 
-            document.add(
-                    new Paragraph(" "));
+        applicantTable.addCell(
+                headerCell("Name"));
 
-            document.add(
-                    new Paragraph(
-                            "Officer Notes"));
+        applicantTable.addCell(
+                valueCell(
+                        application
+                                .getApplicant()
+                                .getFullName()));
 
-            document.add(
-                    new Paragraph(
-                            application.getReviewNotes() == null
-                                    ? "No notes available"
-                                    : application.getReviewNotes()));
+        applicantTable.addCell(
+                headerCell("Email"));
 
-            document.close();
-        }
-        catch(Exception e)
-        {
-            e.printStackTrace();
-        }
+        applicantTable.addCell(
+                valueCell(
+                        application
+                                .getApplicant()
+                                .getEmail()));
+
+        applicantTable.addCell(
+                headerCell("Phone"));
+
+        applicantTable.addCell(
+                valueCell(
+                        application
+                                .getApplicant()
+                                .getPhone()));
+
+        applicantTable.addCell(
+                headerCell("Address"));
+
+        applicantTable.addCell(
+                valueCell(
+                        application
+                                .getApplicant()
+                                .getAddress()));
+
+        document.add(applicantTable);
+
+        document.add(new Paragraph(" "));
+
+        // =====================================
+        // VEHICLE INFORMATION
+        // =====================================
+
+        document.add(
+                new Paragraph(
+                        "Vehicle Information",
+                        sectionFont));
+
+        PdfPTable vehicleTable =
+                new PdfPTable(2);
+
+        vehicleTable.setWidthPercentage(100);
+
+        vehicleTable.addCell(
+                headerCell("Year"));
+
+        vehicleTable.addCell(
+                valueCell(
+                        String.valueOf(
+                                application
+                                        .getVehicle()
+                                        .getYear())));
+
+        vehicleTable.addCell(
+                headerCell("Make"));
+
+        vehicleTable.addCell(
+                valueCell(
+                        application
+                                .getVehicle()
+                                .getMake()));
+
+        vehicleTable.addCell(
+                headerCell("Model"));
+
+        vehicleTable.addCell(
+                valueCell(
+                        application
+                                .getVehicle()
+                                .getModel()));
+
+        document.add(vehicleTable);
+
+        document.add(new Paragraph(" "));
+
+        // =====================================
+        // LOAN INFORMATION
+        // =====================================
+
+        document.add(
+                new Paragraph(
+                        "Loan Information",
+                        sectionFont));
+
+        PdfPTable loanTable =
+                new PdfPTable(2);
+
+        loanTable.setWidthPercentage(100);
+
+        loanTable.addCell(
+                headerCell("Auto Price"));
+
+        loanTable.addCell(
+                valueCell(
+                        "$" +
+                        application
+                                .getLoan()
+                                .getAutoPrice()));
+
+        loanTable.addCell(
+                headerCell("Down Payment"));
+
+        loanTable.addCell(
+                valueCell(
+                        "$" +
+                        application
+                                .getLoan()
+                                .getDownPayment()));
+
+        loanTable.addCell(
+                headerCell("Interest Rate"));
+
+        loanTable.addCell(
+                valueCell(
+                        application
+                                .getLoan()
+                                .getInterestRate()
+                                + "%"));
+
+        loanTable.addCell(
+                headerCell("Loan Term"));
+
+        loanTable.addCell(
+                valueCell(
+                        application
+                                .getLoan()
+                                .getLoanTerm()
+                                + " Months"));
+
+        loanTable.addCell(
+                headerCell("Sales Tax"));
+
+        loanTable.addCell(
+                valueCell(
+                        application
+                                .getLoan()
+                                .getSalesTax()
+                                + "%"));
+
+        loanTable.addCell(
+                headerCell("Fees"));
+
+        loanTable.addCell(
+                valueCell(
+                        "$" +
+                        application
+                                .getLoan()
+                                .getFees()));
+
+        loanTable.addCell(
+                headerCell("Cash Incentive"));
+
+        loanTable.addCell(
+                valueCell(
+                        "$" +
+                        application
+                                .getLoan()
+                                .getCashIncentive()));
+
+        document.add(loanTable);
+
+        document.add(new Paragraph(" "));
+
+        // =====================================
+        // OFFICER REVIEW
+        // =====================================
+
+        document.add(
+                new Paragraph(
+                        "Officer Review",
+                        sectionFont));
+
+        PdfPTable reviewTable =
+                new PdfPTable(2);
+
+        reviewTable.setWidthPercentage(100);
+
+        reviewTable.addCell(
+                headerCell("Status"));
+
+        reviewTable.addCell(
+                valueCell(
+                        application.getStatus()));
+
+        reviewTable.addCell(
+                headerCell("Officer Notes"));
+
+        reviewTable.addCell(
+                valueCell(
+                        application.getReviewNotes() == null
+                                ? "No notes available"
+                                : application.getReviewNotes()));
+
+        document.add(reviewTable);
+
+        document.close();
     }
+    catch(Exception e)
+    {
+        e.printStackTrace();
+    }
+}
+
+private static PdfPCell headerCell(
+        String text)
+{
+    Font bold =
+            new Font(
+                    Font.HELVETICA,
+                    11,
+                    Font.BOLD);
+
+    return new PdfPCell(
+            new Phrase(
+                    text,
+                    bold));
+}
+
+private static PdfPCell valueCell(
+        String text)
+{
+    return new PdfPCell(
+            new Phrase(text));
+}
+
+
 }
